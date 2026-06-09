bc329b1bbec21606402214931e32d05fd5f89e56
vaishnavi80801 <gawaliv433@gmail.com>
2026-06-09 17:40:59 +0530

    Update CLASSES by removing unused items
    Removed 'tvmonitor', 'pen', and 'notebook' from CLASSES.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ confThresh = 0.2
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor" ,"pen" ,"notebook"]
+	"sofa", "train"]
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 print("Loading model...")
